2e30dd72220eb5b7fcf4a6720f0f9e05a749c293
Praise Innocent <praiseinnocent049@gmail.com>
2026-05-21 16:17:53 +0100

    Initial Portfolio Setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#050816] text-white">
+      <h1 className="text-4xl font-bold">Praise Innocent Portfolio</h1>
+    </main>
+  );
+}
